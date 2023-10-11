c2ce5a8aee4ecadf6880404252fccf9c375855d6
Kupie <kupokraft@gmail.com>
2023-10-10 19:44:47 -0500

    Updated to not have hardcoded URL

--- Donation_Engine_Rewrite.py
+++ Donation_Engine_Rewrite.py
@@ -1,19 +1,22 @@
 #CONFIGURE STUFF HERE
 
+#TrackerURL. Ex "donate.midwestspeedfest.com"
+TrackerURL = "24.199.72.160"
 
 #Tracker Login Info
 trackerUser = "USERNAME_HERE"
 trackerPassword = "PASSWORD_HERE"
 
 #How many seconds in between updates. Don't crank this number too low, bullying servers is rude
-donoRefreshRate = 30
+donoRefreshRate = 15
+
+#Manually center text by adding in spaces?
+manuallyCenterText = True
+
 
 #Should we update donation totals? Set to True/False only
 #currently unimplemented, always does this
 updateTotals = True
-
-#add spaces to donation totals
-spaceCenteredDono = True
 
 #Should we automatically approve donations to send to the host? True/False only
 #currently unimplemented, always does this
@@ -29,8 +32,8 @@ approvePendingDonos = False
 
 
 
-#Event ID in tracker (Go to "edit event" and look at the URL, number between 'event' and 'change'; MWSF2021 is "3" for example: https://tracker.2dcon.net/admin/tracker/event/3/change/
-EventID = 16
+#Event ID in tracker (Go to "edit event" and look at the URL, number between 'event' and 'change'; MWSF2021 is "3" for example: https://tracker.midwestspeedfest.com/admin/tracker/event/1/change/
+EventID = 1
 
 
 #------------END CONFIGURATION AREA--------------
@@ -47,21 +50,22 @@ if sys.version_info < (3, 0):
 import os
 def cls():
 	os.system('cls' if os.name=='nt' else 'clear')
-	pass
 
+if os.name=='nt':
+	os.system('title Donations Engine')
 #Set Window Title. I stuck this under a "try" command because this would fail on Linux systems due to no ctypes/window stuffs
 #This also disables quick edit mode as selecting the window freezes everything
-try:
-	k32 = windll.kernel32
-	k32.SetConsoleTitleW("Donations Engine")
-	k32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
-except:
-	if os.name=='nt':
-		print('Missing module ctypes, somehow... it is packaged in Python these days...')
-		os.system('title Donations Engine')
-	else:
-		pass
-	pass
+#try:
+#	import ctypes
+#	k32 = ctypes.windll.kernel32
+#	k32.SetConsoleTitleW("Donations Engine")
+#	k32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
+#except:
+#	if os.name=='nt':
+#		print('Missing module ctypes, please install using "python -m pip install ctypes" if you care about having a fancy cmd prompt title')
+#	else:
+#		pass
+#	pass
 
 #Import the rest
 import time
@@ -104,92 +108,96 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 	
 #-------------------END IMPORTING AREA---------------------#
 
-requestsSession = None
+#####Intial setup of global vars#######
+#Initial Donation Totals
+DonoTotal = '0'
+
+#Keep session throughout loops
+requestsSession = requests.Session()
+
+
 
 def main():
-
+	global DonoTotal
+	global requestsSession
+	global csrfMiddlewareTokenVar
 	#Did you know we can just use a "session" class in requests and it'll automagically handle storing/retreiving cookies? dafuq, that's amazing
 	#And then it just works using "session.request" instead of "requests.request"
 	# #magic
-	global requestsSession
-	global DonoTotal
-	#Only login once if requestsSession is null
-	if not requestsSession:
-		print('Logging in...')
-		requestsSession = requests.Session()
-		
-		#Initial Donation Totals
-		DonoTotal = '0'
-		
-		url = "https://tracker.2dcon.net/admin/login"
-		
-		payload={}
-		headers = {
-			'Host': 'tracker.2dcon.net'
-		}
-		
-		try:
-			#Make the request to tracker, plus the "goals" and "bidwars" page
-			response = requestsSession.request("GET", url, headers=headers, data=payload, verify=False)
-		except:
-			#If it fails, URL is invalid... or Tracker is down. That's always an option
-			print ("Invalid URL connection to tracker failed. Open this python script and check that 2nd line! Exiting...")
-			sys.exit(1)
-		
-		
+	
+	
+	url = 'https://' + TrackerURL + '/admin/login'
+	
+	payload={}
+	headers = {
+		'Host': TrackerURL
+	}
+	
+	try:
+		#Make the request to tracker, plus the "goals" and "bidwars" page
+		response = requestsSession.request("GET", url, headers=headers, data=payload, verify=False)
+	except:
+		#If it fails, URL is invalid... or Tracker is down. That's always an option
+		print ("Invalid URL connection to tracker failed. Open this python script and check that 2nd line! Exiting...")
+		sys.exit(1)
+	#print(requestsSession.cookies['csrftoken'])
+	#print(response.text)
+	try:
 		csrfMiddlewareTokenVar = htmlGetCsrfMiddlewareToken(response)
-		#print()
-		#print('csrfmiddlewaretoken:')
-		#print(csrfMiddlewareTokenVar)
-		
-		####################################################################################################
-		##################################LOGIN#############################################################
-		####################################################################################################
-		url = "https://tracker.2dcon.net/admin/login/?next=/admin/"
-		
-		
-		#tracker won't accept json content-type for some reason... who knows, I still wanna format things easily in a "dict" first and then this just converts it to urlencoded after
-		payload_json = {
-			'csrfmiddlewaretoken' : csrfMiddlewareTokenVar, 
-			'username' : trackerUser,
-			'password' : trackerPassword,
-			}
-		
-		payload = str()
-		for i in payload_json:
-			payload += i + '=' + payload_json[i] + '&'
-		
-		headers = {
-			'Host': 'tracker.2dcon.net',
-			'Content-Type': 'application/x-www-form-urlencoded', 
-			'Referer': 'https://tracker.2dcon.net/admin/login/?next=/admin/', 
-		
+	except:
+		#print('No new Middleware Token')
+		pass
+	#print()
+	#print('csrfmiddlewaretoken:')
+	#print(csrfMiddlewareTokenVar)
+	
+	####################################################################################################
+	##################################LOGIN#############################################################
+	####################################################################################################
+	url = 'https://' + TrackerURL + '/admin/login/?next=/admin/'
+	
+	
+	#tracker won't accept json content-type for some reason... who knows, I still wanna format things easily in a "dict" first and then this just converts it to urlencoded after
+	payload_json = {
+		'csrfmiddlewaretoken' : csrfMiddlewareTokenVar, 
+		'username' : trackerUser,
+		'password' : trackerPassword,
 		}
-		
-		response = requestsSession.request("POST", url, headers=headers, data=payload, verify=False)
-		#print(response.text)
-		#csrfMiddlewareTokenVar = htmlGetCsrfMiddlewareToken(response)
+	
+	payload = str()
+	for i in payload_json:
+		payload += i + '=' + payload_json[i] + '&'
+	
+	headers = {
+		'Host': TrackerURL,
+		'Content-Type': 'application/x-www-form-urlencoded', 
+		'Referer': 'https://' + TrackerURL + '/admin/login/?next=/admin/', 
+
+	}
+	
+	response = requestsSession.request("POST", url, headers=headers, data=payload, verify=False)
+	#print(response.text)
+	#csrfMiddlewareTokenVar = htmlGetCsrfMiddlewareToken(response)
 	
 	
 	
 	####################################################################################################
 	##################################GET DONATIONS TO APPROVE##########################################
 	####################################################################################################
-	url = "https://tracker.2dcon.net/tracker/api/v1/search/?type=donation&all_comments=&feed=toprocess&event=" + str(EventID)
+	url = 'https://' + TrackerURL + '/api/v1/search/?type=donation&all_comments=&feed=toprocess&event=' + str(EventID)
 	
 	payload={}
 	headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0',
-	'Referer': 'https://tracker.2dcon.net/admin/tracker/donation/process_donations',
+	'Referer': 'https://' + TrackerURL + '/admin/tracker/donation/process_donations',
 	}
 	
 	response = requestsSession.request("GET", url, headers=headers, data=payload, verify=False)
-	
 	####################################################################################################
 	##################################APPROVE LIST OF DONATIONS#########################################
 	####################################################################################################
 	for i in response.json():
-		url = "https://tracker.2dcon.net/tracker/api/v1/edit/"
+		url = 'https://' + TrackerURL + '/api/v1/edit/'
 		donoID = str(i['pk'])
 		try:
 			Donor = str(i['fields']['donor__public'])
@@ -197,9 +205,9 @@ def main():
 			Donor = 'No Donor...lolwut?'
 		payload = "type=donation&id=" + donoID + "&readstate=READY&commentstate=APPROVED"
 		headers = {
-			'Host': 'tracker.2dcon.net',
+			'Host': TrackerURL,
 			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			'Referer': 'https://tracker.2dcon.net/admin/tracker/donation/process_donations',
+			'Referer': 'https://' + TrackerURL + '/admin/tracker/donation/process_donations',
 		}
 		
 		response = requestsSession.request("POST", url, headers=headers, data=payload, verify=False)
@@ -209,9 +217,9 @@ def main():
 	####################################################################################################
 	##################################UPDATE DONATION TOTALS############################################
 	####################################################################################################
-	
-	
-	url = 'https://tracker.2dcon.net/tracker/event/' + str(EventID)
+    
+    
+	url = 'https://' + TrackerURL + '/event/' + str(EventID)
 	try:
 		#Make the request to tracker, plus the "goals" and "bidwars" page
 		page = requests.get(url, verify=False)
@@ -222,34 +230,48 @@ def main():
 	soup = BeautifulSoup(page.content,'html.parser')
 	
 	span = soup.find('h2', class_='text-center')
-	spantext1 = span.text.split('(')
+	#Remove after the parenthesis
+	spantext1 = span.text.split('(')[0]
+
+	#Grab text after the dollar sign
+	spantext2 = spantext1.split('$')[-1]
+	#print(spantext2)
+	#print('############')
 	
-	spantext2 = spantext1[-2].split('$')[-1]
+	#Remove commas
 	spantext3 = spantext2.replace(',', '')
-	spanint = int(spantext3.split(".")[0])
+	#print(spantext3)
+	#print('############')
 	
+	#Remove everything after the decimal
+	spanint = int(spantext3.split(".")[0])
+	#print(spanint)
+	#print('############')
 	
 	if (spanint > int(DonoTotal)):
 		DonoTotal = spanint
 		DonoTotal = str(int(DonoTotal) )
 	
-	if (spaceCenteredDono):
-		#Used for testing different number values
-		#DonoTotal = '2000'
-		if len(DonoTotal) == 1:
-			TotalValue = "     $" + DonoTotal
-		elif len(DonoTotal) == 2:
-			TotalValue = "    $" + DonoTotal
-		elif len(DonoTotal) == 3:
-			TotalValue = "   $" + DonoTotal
-		elif len(DonoTotal) == 4:
-			TotalValue = "  $" + DonoTotal
-		elif len(DonoTotal) == 5:
-			TotalValue = " $" + DonoTotal
-		else:
-			TotalValue = "$" + DonoTotal
-	else:
-		TotalValue = "$" + DonoTotal
+	
+	#Used for testing different number values
+	#DonoTotal = '100'
+	TotalValue = "$" + DonoTotal
+	#Add spaces if text is being manually centered
+	if manuallyCenterText:
+		TotalValue = manualCenterDonoText(TotalValue)
+		
+	#if len(DonoTotal) == 1:
+	#	TotalValue = "     $" + DonoTotal
+	#elif len(DonoTotal) == 2:
+	#	TotalValue = "    $" + DonoTotal
+	#elif len(DonoTotal) == 3:
+	#	TotalValue = "   $" + DonoTotal
+	#elif len(DonoTotal) == 4:
+	#	TotalValue = "  $" + DonoTotal
+	#elif len(DonoTotal) == 5:
+	#	TotalValue = " $" + DonoTotal
+	#else:
+	#	TotalValue = "$" + DonoTotal
 	TotalRaisedText = "Total Raised: $" + DonoTotal
 	text_file = open("Totals.txt", "w")
 	
@@ -269,6 +291,12 @@ def htmlGetCsrfMiddlewareToken(requestResponse):
 	csrfmiddlewaretoken = soup.find('input', attrs={'name':'csrfmiddlewaretoken', 'type':'hidden'}).get('value')
 	return csrfmiddlewaretoken
 
+def manualCenterDonoText(totalString):
+	#Buffer it out to be 6 characters long
+	spacesToAdd = 6 - len(totalString)
+	for x in range(0,spacesToAdd):
+		totalString = ' ' + totalString
+	return totalString
 
 if __name__ == '__main__':
 	#Set up a log file
@@ -283,12 +311,10 @@ if __name__ == '__main__':
 				time.sleep(donoRefreshRate)
 			except KeyboardInterrupt:
 				print('Ctrl+C has been hit, exiting script...')
-				time.sleep(2)
 				sys.exit(1)
 				
-			except Exception as e:
-				print(e)
-				print('Donation Script failed with above error... restarting in ' + str(donoRefreshRate) + 'seconds!')
+			except:
+				print('Donation Script failed somehow... restarting in ' + str(donoRefreshRate) + 'seconds!')
 				file = open('Donation_Engine_Rewrite_log.txt', 'a')
 				traceback.print_exc(file=file)
 				file.close()
